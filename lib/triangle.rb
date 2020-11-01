cc058a60c8e7e822d1fe5490a44b2b69f7b94f3c
Carolyn Manning <carolyn.manning3@gmail.com>
2020-11-01 03:30:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     sides = [side_1, side_2, side_3].sort
     if sides.any? {|side| side <= 0} || sides.all? {|side| side == 0}
       raise TriangleError
-    elsif side_1 + side_2 < side_3 || side_1 + side_2 < side_3
+    elsif sides[0] + sides[1] < sides[2]
     elsif sides.uniq.length == 1
       :equilateral
     elsif sides.uniq.length == 2
